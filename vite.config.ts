0eda19c79e721cf55f0181e33c26e37aad98c17c
truongphucthinhtg1011-prog <truongphucthinhtg.1011@gmail.com>
2026-09-05 19:41:32 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/lichbaogiang/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
